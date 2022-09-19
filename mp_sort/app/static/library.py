47ea083597a50994dc3b1b660ff7571752e79178
Sean Phay <seanphay@gmail.com>
2022-09-19 15:38:03 +0800

    fix: unnecessary lines

--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -60,33 +60,20 @@ def sortnumber2():
 	''' This function is used in Exercise 2.
 
 	The function is called when the sort button is clicked.
-
-
-
 	You need to do the following:
-
 	- Get the numbers from a string variable "value".
-
 	- Split the string using comma as the separator and convert them to
-
 	a list of numbers
-
 	- call your sort function, either bubble sort or insertion sort
-
 	- create a string of the sorted numbers and store it in array_str
-
 	'''
 
 	# The following line get the value of the text input called "numbers"
-
 	# value = document.getElementsByName("numbers")[0].value
-
 	value = '20,30,50,40'
-
 	# Throw alert and stop if nothing in the text input
 
 	if value == "":
-
 		window.alert("Your textbox is empty")
 		pass
 
